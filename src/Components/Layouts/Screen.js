a42cbf9b8457cb5a66df2508f170c47aacc3372a
cactis <cactis.lin@gmail.com>
2019-10-20 12:12:38 +0800

    screen pull to refresh

--- src/Components/Layouts/Screen.js
+++ src/Components/Layouts/Screen.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import { Scroll, Grid, SafeArea } from './'
-
+import { RefreshControl } from 'react-native'
 export class Screen extends React.Component {
-    render() {
-        let {
-            padding = 10,
-            safeAreaDisabled = false,
-            scrollable = false,
-        } = this.props
-        const content = scrollable ? (
-            <Scroll>
-                <Grid padding={padding} {...this.props} />
-            </Scroll>
-        ) : (
-            <Grid padding={padding} {...this.props} />
-        )
-        const body = safeAreaDisabled ? (
-            content
-        ) : (
-            <SafeArea flex={1}>{content}</SafeArea>
-        )
-        return body
-    }
+  state = {
+    refreshing: false,
+  }
+  onRefresh = () => {
+    this.props.onRefresh && this.props.onRefresh()
+  }
+
+  render() {
+    let { refreshing } = this.state
+    let {
+      padding = 10,
+      safeAreaDisabled = false,
+      scrollable = false,
+    } = this.props
+    const content = scrollable ? (
+      <Scroll
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={this.onRefresh} />
+        }
+      >
+        <Grid padding={padding} {...this.props} />
+      </Scroll>
+    ) : (
+      <Grid padding={padding} {...this.props} />
+    )
+    const body = safeAreaDisabled ? (
+      content
+    ) : (
+      <SafeArea flex={1}>{content}</SafeArea>
+    )
+    return body
+  }
 }
