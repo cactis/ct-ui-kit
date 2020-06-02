c4e8921b6ac0d6c5770e3d45747aa9001e541bcd
cactis <cactis.lin@gmail.com>
2020-06-02 18:23:00 +0800

    dummy

---
+++ src/Components/Elements/DevFlag.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+
+let _navigation
+export class DevFlag extends React.PureComponent {
+  state = {
+    data: null,
+  }
+
+  render() {
+    let { data } = this.state
+    log(data, 'data in DevFlag render()')
+    // if (!data) return null
+    // let { item = data} = data
+    return window.host == 'dev' ? (
+      <T.Float
+        bottom={4 * SIZE.l}
+        right={-1 * SIZE.t}
+        backgroundColor="rgb(1, 62, 76)"
+        borderRadius={SIZE.t}
+        padding={SIZE.s}
+      >
+        <T.Label text="dev" theme="H9" color="white" />
+      </T.Float>
+    ) : null
+  }
+  onPress = () => {
+    if (this.props.onPress) {
+      this.props.onPress()
+    } else {
+      log('need to set onPress on item')
+    }
+  }
+
+  initStateData = (onComplete) => {
+    let { data } = this.props
+    this.mounted &&
+      this.setState({ data }, () => {
+        onComplete && onComplete()
+      })
+  }
+
+  componentDidMount() {
+    _trace()
+    this.mounted = true
+    _navigation = this.props.navigation
+    this.initStateData(() => {
+      this.autoRun()
+    })
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.navigation !== this.props.navigation)
+      _navigation = this.props.navigation
+  }
+
+  componentWillUnmount() {
+    this.mounted = false
+  }
+  autoRun = () => {}
+}
+var styles = StyleSheet.create({})
