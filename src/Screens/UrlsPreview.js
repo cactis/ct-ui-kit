56f6a646ede4b6433d9106f6d333c28a8de98118
cactis <cactis.lin@gmail.com>
2020-12-03 17:32:10 +0800

    dummy

--- src/Screens/UrlsPreview.js
+++ src/Screens/UrlsPreview.js
@@ -13,34 +13,34 @@ let { data } = this.state
 log(data, 'data in UrlsPreview render()')
 if (!data) return null
 let { item = data} = data
+let space = item.length == 0 ? null :  <T.Space size={SIZE.s} />
+  return <>{space}{item.length == 1 ? <T.UrlPreviewItem
+    itemWidth='100%'
+    navigation={_navigation} data={item[0]}
+    parent={this}
+  /> : (
+      <T.List
+        horizontal
+        ref={c => this.list = c}
+        data={item}
+        // bordered
+        // style={{borderWidth: 3}}
+        // padding={SIZE.n}
+        contentContainerStyle={{ paddingBottom: SIZE.s }}
+        // columnWrapperStyle={{ border: 1px solid #333, borderWidth: 1}}
+        // padding={SIZE.s}
+        ItemSeparatorComponent={() => <T.Space backgroundColor__='red' />}
+        renderItem={_item => (
+          <T.UrlPreviewItem
+            itemWidth={item.length > 1 ? SCREEN_WIDTH * (__DEV__ && false ? 0.3 : 0.85) : SCREEN_WIDTH * 0.98}
+            navigation={_navigation}
 
-return item.length == 1 ? <T.UrlPreviewItem 
-itemWidth='100%'
-navigation={_navigation} data={item[0]}
-parent={this}
-/> : (
-  <T.List
-    horizontal
-ref={c => this.list = c}
-    data={item}
-      // bordered
-      // style={{borderWidth: 3}}
-      // padding={SIZE.n}
-      contentContainerStyle={{paddingBottom: SIZE.s}}
-    // columnWrapperStyle={{ border: 1px solid #333, borderWidth: 1}}
-    // padding={SIZE.s}
-    ItemSeparatorComponent={()=><T.Space  backgroundColor__='red'/>}
-renderItem={_item => (
-  <T.UrlPreviewItem 
-    itemWidth={item.length > 1 ? SCREEN_WIDTH* (__DEV__ && false ? 0.3 : 0.85) : SCREEN_WIDTH * 0.98}
-navigation={_navigation}
-
-data={_item}
-parent={this}
-/>
-)}
-/>
-)
+            data={_item}
+            parent={this}
+          />
+        )}
+      />
+    )}</>
 }
 onPress = () => {
 if (this.props.onPress){
